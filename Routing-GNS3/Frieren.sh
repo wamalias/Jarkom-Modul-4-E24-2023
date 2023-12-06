16da567b438470d61546b9eaf07488788cb4583f
wamalias <111508713+wamalias@users.noreply.github.com>
2023-12-06 15:59:50 +0700

    Create Frieren.sh

---
+++ Routing-GNS3/Frieren.sh
@@ -0,0 +1,5 @@
+route add -net 192.218.0.4 netmask 255.255.255.252 gw 192.218.0.10
+route add -net 192.218.0.0 netmask 255.255.255.252 gw 192.218.0.10
+route add -net 192.218.8.0 netmask 255.255.252.0 gw 192.218.0.10
+route add -net 192.218.24.0 netmask 255.255.248.0 gw 192.218.0.10
+route add -net 192.218.0.40 netmask 255.255.255.248 gw 192.218.0.10
