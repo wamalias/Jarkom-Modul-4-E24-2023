ee80f15dcabfec32909634ac9fa35b389d35db09
wamalias <111508713+wamalias@users.noreply.github.com>
2023-12-06 15:53:56 +0700

    Create Aura.sh

---
+++ Routing/Aura.sh
@@ -0,0 +1,20 @@
+route add -net 192.218.0.64 netmask 255.255.255.224 gw 192.218.0.14
+route add -net 192.218.0.8 netmask 255.255.255.252 gw 192.218.0.14
+route add -net 192.218.0.4 netmask 255.255.255.252 gw 192.218.0.14
+route add -net 192.218.0.0 netmask 255.255.255.252 gw 192.218.0.14
+route add -net 192.218.8.0 netmask 255.255.252.0 gw 192.218.0.14
+route add -net 192.218.24.0 netmask 255.255.248.0 gw 192.218.0.14
+route add -net 192.218.0.40 netmask 255.255.255.248 gw 192.218.0.14
+
+route add -net 192.218.1.0 netmask 255.255.255.0 gw 192.218.0.38
+
+route add -net 192.218.0.48 netmask 255.255.255.248 gw 192.218.0.18
+route add -net 192.218.0.24 netmask 255.255.255.252 gw 192.218.0.18
+route add -net 192.218.0.28 netmask 255.255.255.252 gw 192.218.0.18
+route add -net 192.218.0.32 netmask 255.255.255.252 gw 192.218.0.18
+route add -net 192.218.0.128 netmask 255.255.255.192 gw 192.218.0.18
+route add -net 192.218.16.0 netmask 255.255.252.0 gw 192.218.0.18
+route add -net 192.218.0.20 netmask 255.255.255.252 gw 192.218.0.18
+route add -net 192.218.12.0 netmask 255.255.252.0 gw 192.218.0.18
+route add -net 192.218.2.0 netmask 255.255.255.0 gw 192.218.0.18
+route add -net 192.218.4.0 netmask 255.255.254.0 gw 192.218.0.18
