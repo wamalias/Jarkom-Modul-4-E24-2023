51fd558db2399dee7f7ef7ae04afc96d51933259
wamalias <111508713+wamalias@users.noreply.github.com>
2023-12-06 15:59:23 +0700

    Create Eisen.sh

---
+++ Routing-GNS3/Eisen.sh
@@ -0,0 +1,7 @@
+route add -net 192.218.12.0 netmask 255.255.252.0 gw 192.218.0.30
+route add -net 192.218.2.0 netmask 255.255.255.0 gw 192.218.0.30
+
+route add -net 192.218.0.20 netmask 255.255.255.252 gw 192.218.0.26
+route add -net 192.218.4.0 netmask 255.255.254.0 gw 192.218.0.26
+route add -net 192.218.0.128 netmask 255.255.255.192 gw 192.218.0.26
+route add -net 192.218.16.0 netmask 255.255.255.0 gw 192.218.0.26
